fc01c6ea747366d137e1134c1dc5119b95b59adc
André Gomes <45216757+AndreLSGomes@users.noreply.github.com>
2023-03-29 11:07:05 -0300

    Add checagem se diretorio existe para salvar stickers

--- alura-stickers/src/GeradoraDeFigurinhas.java
+++ alura-stickers/src/GeradoraDeFigurinhas.java
@@ -1,10 +1,17 @@
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
+import java.awt.BasicStroke;
+import java.awt.Shape;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
 
 import javax.imageio.ImageIO;
 
@@ -35,8 +42,6 @@ public class GeradoraDeFigurinhas {
         // Obtendo altura e largura da imagem
         int largura = imagemOriginal.getWidth();
         int altura = imagemOriginal.getHeight();
-
-        // Setando nova altura da imagem
         int novaAltura = altura + 200;
         
         // Criando a nova imagem
@@ -47,20 +52,50 @@ public class GeradoraDeFigurinhas {
         graphics.drawImage(imagemOriginal, 0, 0, null);
 
         // Setando a fonte para escrever na imagem
-        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 84);
-        graphics.setColor(Color.YELLOW);
-        graphics.setFont(fonte);
+        var font = new Font("Impact", Font.BOLD, 80 );
+        graphics.setFont(font);
+        graphics.setColor(Color.yellow);
 
-        // Escrever na nova Imagem de acordo com a classificação do filme
-        if(rating >= 8.0) {
-            graphics.drawString("TOPZERA", 350, novaAltura - 70);
-        } else if (rating >= 7.0 && rating < 8.0) {
-            graphics.drawString("NICE", 350, novaAltura - 70);
+        // Setando o texto a ser escrito na imagem
+        String word = "";
+
+        if(rating >= 9.0) {
+            word = "TOPZERA";
+        } else if(rating >= 8.0 && rating < 9.0) {
+            word = "LEGAL";
         } else {
-            graphics.drawString("BLAH!", 350, novaAltura - 70);
+            word = "MAOMENOS";
         }
 
-        ImageIO.write(novaImagem, "png", new File("saida/" + nomeArquivo));
+        FontMetrics fontMetrics = graphics.getFontMetrics();
+        Rectangle2D rectangle   = fontMetrics.getStringBounds(word, graphics);
+        int wordWidth           = (int)rectangle.getWidth();
+        int wordPositionX       = (largura - wordWidth) / 2;
+        int wordPositionY       = (novaAltura - 100);
+
+        // Escrevendo na nova imagem
+        graphics.drawString(word, wordPositionX, wordPositionY);
+
+        FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+        var textLayout = new TextLayout(word, font, fontRenderContext);
+        Shape outline = textLayout.getOutline(null);
+        AffineTransform transform = graphics.getTransform();
+        transform.translate(wordPositionX, wordPositionY);
+        graphics.setTransform(transform);
+        var outlineStroke = new BasicStroke(largura * 0.004f);
+        graphics.setStroke(outlineStroke);
+        graphics.setColor(Color.BLACK);
+        graphics.draw(outline);
+        graphics.setClip(outline);
+
+        // Setando o diretório onde serão armazenadas as imagens
+        String saida = "saida/";
+
+        File dir = new File(saida);
+        if (!dir.exists()) {
+            new File(saida).mkdir();
+        }
+        ImageIO.write(novaImagem, "png", new File(saida + nomeArquivo));
     }
     
 }
